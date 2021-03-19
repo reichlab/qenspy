1bc1876ee9b7a5f42c31af01014a318c8f688a8e
Evan Ray <elray@umass.edu>
2021-03-19 14:16:50 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='qenspy',
+    version='0.0.1',
+    description='Quantile ensemble models',
+    author='Serena Wang, Evan L. Ray',
+    author_email='elray@umass.edu',
+    url='https://github.com/reichlab/qenspy',
+    packages=['qenspy'],
+)
